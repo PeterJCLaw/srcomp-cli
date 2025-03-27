da1d53f2cebd9bf41fe82ff3afc88037d64cef4a
Peter Law <PeterJCLaw@gmail.com>
2025-03-27 15:46:21 +0000

    Factor out core logic of knocked-out-teams to clarify code
    This also makes it easier to introduce other functionality I'm about
    to add.

--- sr/comp/cli/knocked_out_teams.py
+++ sr/comp/cli/knocked_out_teams.py
@@ -1,9 +1,26 @@
 from __future__ import annotations
 
 import argparse
+import dataclasses
 from pathlib import Path
+from typing import Iterator
 
+from sr.comp.comp import SRComp
+from sr.comp.knockout_scheduler import UNKNOWABLE_TEAM
 from sr.comp.types import TLA
+
+
+@dataclasses.dataclass(frozen=True)
+class Round:
+    number: int
+    name: str
+    teams_this_round: frozenset[TLA | None]
+
+    prior_rounds_complete: bool
+    """
+    Whether the scoring for all the matches prior to ths round has completed and
+    thus the knockouts for this round are completely known.
+    """
 
 
 def round_name(rounds_left: int) -> str:
@@ -16,23 +33,32 @@ def round_name(rounds_left: int) -> str:
     return ""
 
 
-def command(settings: argparse.Namespace) -> None:
-    from sr.comp.comp import SRComp
-    from sr.comp.knockout_scheduler import UNKNOWABLE_TEAM
+def teams_and_rounds(comp: SRComp) -> Iterator[Round]:
+    rounds = comp.schedule.knockout_rounds
 
-    comp = SRComp(settings.compstate)
-
-    teams_last_round: set[TLA | None] = set()
-    last_round_num = len(comp.schedule.knockout_rounds) - 1
-    for i, matches in enumerate(comp.schedule.knockout_rounds):
+    last_round_num = len(rounds) - 1
+    for i, matches in enumerate(rounds):
         teams_this_round = set()
         for game in matches:
             teams_this_round.update(game.teams)
 
-        print(f"## Teams not in round {i} ({round_name(last_round_num - i)})")
+        yield Round(
+            i,
+            round_name(last_round_num - i),
+            frozenset(teams_this_round),
+            prior_rounds_complete=UNKNOWABLE_TEAM not in teams_this_round,
+        )
+
+
+def command(settings: argparse.Namespace) -> None:
+    comp = SRComp(settings.compstate)
+
+    teams_last_round: frozenset[TLA | None] = frozenset()
+    for round_info in teams_and_rounds(comp):
+        print(f"## Teams not in round {round_info.number} ({round_info.name})")
         print()
 
-        if UNKNOWABLE_TEAM in teams_this_round:
+        if not round_info.prior_rounds_complete:
             if settings.force:
                 print("Warning: ", end='')
 
@@ -45,12 +71,12 @@ def command(settings: argparse.Namespace) -> None:
             if not settings.force:
                 return
 
-        out = teams_last_round - teams_this_round
+        out = teams_last_round - round_info.teams_this_round
         teams_out = [t for t in out if t is not None]
         for tla in sorted(teams_out):
             print(tla, comp.teams[tla].name)
 
-        teams_last_round = teams_this_round
+        teams_last_round = round_info.teams_this_round
         print()
 
 
